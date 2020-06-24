fb34e1b054cd819bbe9eeaec97616c6da4d82582
heitor-ps <heytorpg7@gmail.com>
2020-06-24 19:33:58 -0300

    Primeiro commit

---
+++ isPrimo.rb
@@ -0,0 +1,36 @@
+
+
+
+
+def isPrimo(n)  
+end
+
+n = 4564651;
+a = [];
+for i in 1..n
+    if(n%i==0)    
+       a<<i;     
+    end
+end
+
+
+puts "Divisores de n são #{a}"
+
+#if(a.length()==2)
+   #puts a[1]  
+#else return puts "não é primo"
+#end
+
+
+
+    
+
+
+
+  
+
+
+   
+
+
+
