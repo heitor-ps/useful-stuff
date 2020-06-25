686d67ae844f4106ebe725e3c06f84c5868447c2
heitor-ps <heytorpg7@gmail.com>
2020-06-25 20:45:01 -0300

    Utilizando isPrimo para somar quadrados, fazer diferenças etc

---
+++ soma-potencia-diferenca.rb
@@ -0,0 +1,25 @@
+
+    def soma_dos_quadrados()
+        arrayPrimos = [];
+        contadorPrimos()
+    end
+
+    def diferenca()
+        arrayPrimos = [];
+        contadorPrimos()
+    end
+
+    #this part works as intend, need to iterate.
+    i = 0;
+
+
+
+    i = 0;
+
+    puts arrayPrimos[i] + arrayPrimos[i+=1]
+    i+=1;
+    puts i;
+    soma =  arrayPrimos[i] + arrayPrimos[i+=1]
+
+    puts soma;
+   
