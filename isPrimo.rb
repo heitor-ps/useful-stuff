5f9b1c7231647077f462c7d861131cd82532df22
heitor-ps <heytorpg7@gmail.com>
2020-06-25 00:28:28 -0300

    Função lista os divisores e verifica se N é primo

--- isPrimo.rb
+++ isPrimo.rb
@@ -4,29 +4,17 @@ def isPrimo(n)
    #Essa parte faz os restos de divisão e armazena os restos zero num array
    for i in 1..n
     if(n%i==0)    
-       a<<i;     
+       a<<i; 
     end
    end
 
    #aqui é verificado se o número é primo pelo tamanho do array
    if(a.length()==2)
-   return puts "#{a[1]} é primo :D"
-   else return puts "#{n} não é primo >:("
+   return puts "Divisores de #{n}: #{a}. Logo, #{n} é primo."
+   else return puts "Divisores de #{n}: #{a}. Logo, #{n} não é primo."
    end
-#askaposka
+
+#modificar para que a função possa contar quantos primos foram encontrados
 end
-isPrimo(11);
 
-
-
-    
-
-
-
-  
-
-
-   
-
-
-
+isPrimo(56);
