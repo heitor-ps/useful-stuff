8fa4858c11372a2730c801f9e7beecc2846993a6
heitor-ps <heytorpg7@gmail.com>
2020-06-25 01:38:25 -0300

    Função contando corretamente os primos

--- isPrimo.rb
+++ isPrimo.rb
@@ -1,17 +1,38 @@
+
+
+
 def isPrimo(n)   
    a = [];
-
    #Essa parte faz os restos de divisão e armazena os restos zero num array
    for i in 1..n
     if(n%i==0)    
        a<<i;     
     end
    end
-
    #aqui é verificado se o número é primo pelo tamanho do array
+result = [];
    if(a.length()==2)
-   return puts "#{a[1]} é primo :D"
-   else return puts "#{n} não é primo >:("
+      result<<a[1];
+   else return false
    end
+
+   return result;
 end
-isPrimo(11);
+
+def contadorPrimos(j)
+   n = 1;
+   arrayResultado = [];
+   while arrayResultado.size < j
+   if isPrimo(n)!= false
+      arrayResultado<<isPrimo(n)
+      n+=1
+   else n+=1
+   end  
+end
+   return puts arrayResultado
+end
+
+contadorPrimos(4);
+
+
+
