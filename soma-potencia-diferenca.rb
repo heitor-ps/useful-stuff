b8baa08511212c5d45ebb0cbf584f9ac9bb02f3d
heitor-ps <heytorpg7@gmail.com>
2020-06-27 18:01:03 -0300

    Acesso a contador_primos() feito por variável. Indentação corrigida

--- soma-potencia-diferenca.rb
+++ soma-potencia-diferenca.rb
@@ -1,71 +1,70 @@
 def contadorPrimos(j)
-    def isPrimo(n)
-        a = [];
-        #Essa parte faz os restos de divisão e armazena os restos zero num array
-        for i in 1..n
+   def isPrimo(n)
+      a = [];
+      #Essa parte faz os restos de divisão e armazena os restos zero num array
+      for i in 1..n
          if(n%i==0)    
             a<<i;     
          end
-        end
-    
-        #aqui é verificado se o número é primo pelo tamanho do array
-        result = 0;
-        if(a.length()==2)
-           result=a[1];
-        else return false
-        end
-        return result
-     end
-     
-        #os números primos são armazenados na variavel resultado
-         n = 1;   
-        resultado = [];
-        counter = 0;
-     while counter < j
-        if isPrimo(n)!= false
-       resultado<<isPrimo(n)
-       n+=1
-       counter+=1
+      end
+ 
+      #aqui é verificado se o número é primo pelo tamanho do array
+      result = 0;
+      if(a.length()==2)
+         result=a[1];
+      else return false
+      end
+      return result
+   end
+ 
+   #os números primos são armazenados na variavel resultado
+   n = 1;   
+   resultado = [];
+   counter = 0;
+   while counter < j
+      if isPrimo(n)!= false
+         resultado<<isPrimo(n)
+         n+=1
+         counter+=1
       else n+=1
-    end  
- end
-    return resultado
- end
-class Quadrados
-    def initialize(number)
-    @number = number;
-    end
-
-    def quadrado_da_soma()   
+      end  
+   end
+   return resultado
+end
+ 
+ class Quadrados
+   def initialize(number)
+      @contadorPrimos = contadorPrimos(number);
+   end
+   
+   def quadrado_da_soma()   
       i = 0;    
       soma = 0;        
       arrayPrimos = [];       
-      arrayPrimos = contadorPrimos(@number)        
-        while i < arrayPrimos.size
+      arrayPrimos = @contadorPrimos;
+      while i < arrayPrimos.size
          soma += arrayPrimos[i]
          i+=1        
-        end
-       return soma*soma         
-    end
-
-    def soma_dos_quadrados()
+      end
+      result = soma * soma
+      @quadradoDaSoma = result
+      return result
+   end
+ 
+   def soma_dos_quadrados()
       i = 0;
       soma = 0;
       arrayPrimos = [];
-      arrayPrimos = contadorPrimos(@number)
-
+      arrayPrimos = @contadorPrimos;
       while i < arrayPrimos.size
          soma += arrayPrimos[i]*arrayPrimos[i]
          i+=1         
       end
-
+      @somaDosQuadrados = soma;
       return soma;
-
-      
-    end
-
-    def diferenca()
+   end
+ 
+   def diferenca()
       return  quadrado_da_soma() - soma_dos_quadrados()
-    end
-
-end
+   end
+ end
