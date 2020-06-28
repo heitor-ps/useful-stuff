aa4e3334a29118bf064b6a342437ae01264563b0
heitor-ps <heytorpg7@gmail.com>
2020-06-28 03:10:08 -0300

    Funções finalzadas e adição de verificador de duplicatas para as ending strips (precisa ser modificado)

--- ruby-stuff/rna_polymerase.rb
+++ ruby-stuff/rna_polymerase.rb
@@ -1,6 +1,6 @@
-class RnaPolymerase
+class RnaPolymerase  
   def stripTranslation(strip)
-    result = "";
+    result = "";  
       for i in 0..strip.size
         if(strip[i]=="A")
           result += "U"
@@ -9,60 +9,55 @@ class RnaPolymerase
         if(strip[i]=="T")
           result+="A"
         end
-
+  
         if(strip[i]=="G")
           result += "C"
         end
-
+  
         if(strip[i]=="C")
           result += "G"
-        end
+        end  
       end
   return result
   end
 
   def transcribe(dna, beginning, ending)
-    if(beginning.size <= 1|| ending.size <= 1 || dna.size <= 1)
+    if(beginning.size <= 1|| ending.size <= 1 || dna.size <= 1)      
       return "Sequence not found"
     else
       checkVar = [];
       for j in 0..beginning.size
         if(beginning[0]==beginning[j])
           checkVar<<beginning[j]
-        end
-      end
+        end        
+      end 
       if(checkVar.size==beginning.size)
         return "Sequence not found"
       else
+        #beginning's index must be lower than ending's
         if (dna.index(ending)<dna.index(beginning))
+          endingAux = dna.index(ending);        
+          endingAux += ending.size;
+          firstEndingIndex = dna.index(ending,endingAux);
 
-
-          puts dna;
-          puts "Main strip";
-          puts "\n";
-          puts beginning;
-          puts "Index of beginning: #{dna.index(beginning)}"
-          puts "\n";
-          puts ending;
-          puts "Index of ending: #{dna.index(ending)}";
-          puts "\n"
-          beginningIndex = dna.index(ending);
-          beginningIndex-=1;
-          endingIndex = dna.index(beginning);
-          endingIndex+=1;
-          stripForTranscription = dna[beginningIndex..endingIndex]
+          #This second part is the same as seen on the "else" below with the
+          #difference on endingIndex
+          beginningIndex = dna.index(beginning);
+          beginningIndex+=beginning.size;
+          endingIndex = firstEndingIndex;
+          endingIndex -= 1;
+          stripForTranscription =  dna[beginningIndex..endingIndex];
           result = stripTranslation(stripForTranscription);
-
-          return result
+          return result   
         else
           beginningIndex = dna.index(beginning);
           beginningIndex+=beginning.size;
           endingIndex = dna.index(ending);
-          endingIndex -= 1;
+          endingIndex -= 1;            
           stripForTranscription =  dna[beginningIndex..endingIndex];
           result = stripTranslation(stripForTranscription);
-          return result;
-        end
+          return result; 
+        end         
       end
     end
   end
